5028d50d8aa3e5c5f4ffc67afcf374e659cc0ffc
Shigeyuki Isorena <workmac@Shigeyukis-MacBook-Pro.local>
2023-03-31 01:08:31 -0700

    I added the home page

--- Game pieces.js
+++ Game pieces.js
@@ -1,5 +1,14 @@
-let lion = {
+/*let lion = {
     image
     
+}*/
+import { shogiBoard } from "./main";
+
+function newImage(url, name){
+    let image = document.createElement('img');
+    image.src = url;
+    image.id = name;
+    image.appendChild(shogiBoard)
 }
 
+newImage('./assets/Chick.png', 'chick')
--- main.js
+++ main.js
@@ -2,19 +2,23 @@
 var shogiBoard
 var testText
 let x = 0
+let shogiSquare
 // this is the function for making the board
 function generateBoard(){
     shogiBoard = document.querySelector('#boardGame')
     for (var x = 0; x < 3; x++){
         for(var y= 0; y < 4; y++){
-            let shogiSquare = document.createElement('div');
+            shogiSquare = document.createElement('div');
             shogiSquare.className = 'shogi-Square';
             shogiSquare.style.backgroundColor = '#121';
             console.log(shogiSquare)
             console.log(shogiBoard)
             shogiBoard.appendChild(shogiSquare);
+            return shogiSquare;
         }
         //shogiBoard.appendChild(shogiSquare);
     }
 }
-document.addEventListener('DOMContentLoaded', generateBoard)
+document.addEventListener('DOMContentLoaded', generateBoard);
+
+export {shogiBoard};
